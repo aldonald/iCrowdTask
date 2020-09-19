0209542010f8db0567785f5094beaac4a5fbec8a
Alex Donald <aldon80@gmail.com>
2020-09-19 11:47:07 +1000

    Add files for deployment

--- build/precache-manifest.d6732295214cfccb6fcb236209a75e0d.js
+++ build/precache-manifest.d6732295214cfccb6fcb236209a75e0d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d06e6dea117405055720b4ec2d63a4b",
+    "revision": "debd8d3e96eddc3f94de8eab1ac46ebf",
     "url": "/index.html"
   },
   {
-    "revision": "c6cde47a6cb89a3527f1",
+    "revision": "63352a22869b2029cb80",
     "url": "/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "c6cde47a6cb89a3527f1",
-    "url": "/static/js/2.e2e4c640.chunk.js"
+    "revision": "63352a22869b2029cb80",
+    "url": "/static/js/2.87bfb2a9.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.e2e4c640.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.87bfb2a9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "80f51da8e926fcccc00c",
-    "url": "/static/js/main.20445095.chunk.js"
+    "revision": "4ea8463350745bd551e8",
+    "url": "/static/js/main.0ca28769.chunk.js"
   },
   {
     "revision": "80390516e43dcfe92b81",
---
+++ icrowdtask/build/precache-manifest.d6732295214cfccb6fcb236209a75e0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debd8d3e96eddc3f94de8eab1ac46ebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63352a22869b2029cb80",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "63352a22869b2029cb80",
+    "url": "/static/js/2.87bfb2a9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.87bfb2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea8463350745bd551e8",
+    "url": "/static/js/main.0ca28769.chunk.js"
+  },
+  {
+    "revision": "80390516e43dcfe92b81",
+    "url": "/static/js/runtime-main.f2c23301.js"
+  }
+]);
